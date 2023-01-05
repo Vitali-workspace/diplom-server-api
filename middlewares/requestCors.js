fc4f366cdbddb9bf13dffb73e947a18a9488f7d0
vitali <vitali.slepnyov@gmail.com>
2023-01-05 23:26:08 +0300

    refactor: обновить работу cors запросов

--- middlewares/requestCors.js
+++ middlewares/requestCors.js
@@ -1,39 +1,6 @@
-// const allowedCors = [
-//   'https://frontend.diplom.vitali.nomoredomains.club',
-//   'https://api.diplom.vitali.nomoredomains.club',
-//   'https://api.nomoreparties.co/beatfilm-movies',
-//   'http://frontend.diplom.vitali.nomoredomains.club',
-//   'http://api.diplom.vitali.nomoredomains.club',
-//   'http://api.nomoreparties.co/beatfilm-movies',
-//   'https://praktikum.tk',
-//   'http://praktikum.tk',
-//   'http://localhost:3000',
-//   'https://localhost:3000',
-//   'http://localhost:3001',
-//   'https://localhost:3001',
-//   'localhost:3000',
-//   'http://localhost:3001/signup',
-//   'http://localhost:3000/signup',
-//   'http://localhost:3001/movies',
-//   'http://localhost:3000/movies',
-//   'http://localhost:3001/users/me',
-//   'http://localhost:3000/users/me',
-//   'http://localhost:3001/signout',
-//   'http://localhost:3000/signout',
-//   'http://localhost:3001/signin',
-//   'http://localhost:3000/signin',
-// ];
-
 module.exports = function (req, res, next) {
-  // const { origin } = req.headers;
-
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true);
-
-  // if (allowedCors.includes(origin)) {
-  //   res.header('Access-Control-Allow-Origin', origin);
-  //   res.header('Access-Control-Allow-Credentials', true);
-  // }
 
   // обрабатываем предварительный cors запрос
   const { method } = req;
